f2f68064900349224adcedd1db7bfc8517407215
sid-tripathi <46405836+sid-tripathi@users.noreply.github.com>
2020-02-14 13:38:30 -0500

    Update utility.c

--- utility.c
+++ utility.c
@@ -20,10 +20,10 @@ void pauseMyShell()
 	//While enter has not been pressed, sleep
 	while (1)	{
 		printf("Paused, press Enter to continue");
-		sleep(1);
 		if (getchar == "\n")	{
 			break;
 		}
+		sleep(1);
 	}
 }
 
