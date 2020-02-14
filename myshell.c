f3edfb08088734ddae5fc4771ae226f80655c3d2
HaraseesG <45112294+HaraseesG@users.noreply.github.com>
2020-02-14 11:25:25 -0500

    Update myshell.c

--- myshell.c
+++ myshell.c
@@ -1,75 +1,111 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-//#include <dirent.h>
-//#include <sys/types.h>
 #include <string.h>
+#include <sys/types.h>
+#include <dirent.h>
+
 #include "utility.h"
 #include "myshell.h"
 
+// Length of buffer
+#define BUFFER_LEN 256
 
-int main(int argc, char const *argv[])
+//Return file if there is a file to be read otherwise the standard input stream
+FILE * stream(FILE * file)
 {
-	//print current working directory
-	char cwd[100];
-	if(getcwd(cwd, sizeof(cwd)) != NULL) {
-		printf("%s\n", cwd);
+	if (file != NULL)
+	{
+		return file;
+	}else
+	{
+		return stdin;
+	}
+}
+
+int main(int argc, char *argv[])
+{
+// Init 
+	char buffer[BUFFER_LEN] = {0};
+	char command[BUFFER_LEN] = {0};
+	char args[BUFFER_LEN][BUFFER_LEN] = {{0}};
+	char path[1024]
+	FILE *fp;
+
+	// If there are 2 or more parameters passed, open script
+	if (argc >= 2)
+	{
+		if(strstr(argv[1], ".sh") != NULL)
+		{
+			fp = fopen(argv[1], "r");
+		}
 	}
 
-	//get initial command/string
-	char str[100]; 
-	fgets(str);
-	char command[10];
-	char arg[100];
+	// Print current directory path before user input
+	printf("%s$ ", getcwd(path, sizeof(path)));	
 
-	//loop for command line shell
-	do {
-		//get command (first token from string)
-		char *token = strtok(str, " ");
+	// while there is still user input
+	while (fgets(buffer, BUFFER_LEN, filestream(fp)) != NULL)
+	{
+		// Store each string seperated by a space as a token and then store all tokens in command buffer
+		char* token = strtok(buffer, " ");
 		strcpy(command, token);
 
-		//get argument (second token)
-		while(token != NULL) {
+		// Count tokens
+		int arguments_passed = 0;
+		while (token != NULL)
+		{
 			token = strtok(NULL, " ");
-			if (token != NULL) {
-				strcpy(arg, token);
+			if (token != NULL)
+			{
+				strcpy(args[arguments_passed], token);
+				arguments_passed += 1;
 			}
 		}
 
-		//'cd' command functionality
-		//comparing the command token taken with 'cd'
-		if (strcmp(command, "cd") == 0) {
-			if (strlen(arg <= 0)) {
-				printf("No new directory specified.\n");
-			} else {
-				if(chdir(arg) != 0) {
-					printf("Directory not found\n", stderr);
-				} else {
-					chdir(arg);
-					printf("%s\n", getcwd(cwd, sizeof(cwd)));
+		// Remove noise from arguments like endstring or newline ops
+		for (int i = 0; i < arguments_passed; i++)//Iterate through each argument
+		{
+			for (int j = 0; j < strlen(args[i]); j++)//Iterate through the characters of an individual element at a time
+			{
+				if (args[i][j] == '\n' || args[i][j] == "\0");
+				{
+					args[i][j] = NULL;
 				}
 			}
-			
+		}
+
+		// execute requested command
+
+		// CD- Change Directories
+		if (strcmp(command, "cd") == 0 || strcmp(command, "cd\n") == 0)
+		{
+			if (strlen(args[0]) <= 0)
+			{
+				printf("Current Directory: %s\n", getcwd(path, sizeof(path)));
+			}else
+			{
+				if(chdir(args[0]) < 0)
+				{
+					fputs("Directory does not exist or could not be found.", stderr);
+				}else
+				{
+					chdir(args[0]);
+					printf("Directory: %s\n", getcwd(path, sizeof(path)));
+				}
+			}
+		}
+		// clear- clear
+		else if(strcmp(command, "clear") == 0 || strcmp(command, "clear\n") == 0)
+		{
+			clear();
+		}
+		// dir- Directory
+		else if(strcmp(command, "dir") == 0 || strcmp(command, "dir\n") == 0)
+		{
+
 		}
 
 
-
-
-
-
-
-
-
-
-	}while(token != NULL);
-
-
-
-
-
-
-
-
-
-	return 0;
+	}
 }
