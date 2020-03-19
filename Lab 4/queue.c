d0e7f594e23539eb35b3f156e7107924262c1452
HaraseesG <45112294+HaraseesG@users.noreply.github.com>
2020-03-19 17:03:48 -0400

    Update queue.c

--- Lab 4/queue.c
+++ Lab 4/queue.c
@@ -11,31 +11,27 @@ void push(node_pointer *queue, process process) {
   allocate space with malloc for currentNode -> next to sizeof(node);
   set currentNode -> next -> process to process;
   set currentNode -> next -> next to NULL;*/
-	node_pointer currentNode = queue;
-	while (currentNode.next != NULL) {
-		currentNode = currentNode.next;
+	node_pointer *currentNode = queue;
+	while (currentNode->next != NULL) {
+		currentNode = currentNode->next;
 	}
-	malloc(sizeof(currentNode.next));
-	currentNode.next.process = process;
-	currentNode.next.next = NULL;
+
+	currentNode-> next = (*node_pointer)malloc(sizeof(node_pointer));
+	currentNode->next->process = process;
+	currentNode->next->next = NULL;
 }
 
 process pop(node_pointer *queue) {
-  /*check if queue is empty and if it is exit. If not:
+  /*check if queue has relevant elements and if it is exit. If not:
   create a struct process (as defined in queue.h) and initialize it to queue -> next -> process;
-  create a struct node_pointer (as defined in queue.h) and initalize it to queue -> next
   set queue -> next to queue -> next -> next
-  free node_pointer
   return process*/
-	if (queue == NULL) {
+	if (queue->next == NULL) {
 		printf("queue is empty, exiting.\n");
 		exit(0);
-	} else {
-		struct process newProcess = queue.next.process;
-		//why create this new pointer?
-		struct node_pointer newPointer = queue.next;
-		queue.next = queue.next.next;
-
 	}
+	process newProcess = queue->next->process;
+	
+	queue->next = queue->next->next;
 	return newProcess;
 }
